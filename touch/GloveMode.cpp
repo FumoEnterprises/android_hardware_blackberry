683bf0cad798493616a346211236c207c91e56ee
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-03-02 21:05:56 +0100

    hardware: blackberry: touch: Initialize new HAL
    Change-Id: Ie9739571573a335a6fbf71f6ba3407dc04d1c0d3
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

---
+++ touch/GloveMode.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "GloveModeService"
+
+#include "GloveMode.h"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const std::string kGloveModePath = "/sys/devices/soc/c178000.i2c/i2c-4/4-0070/input/input3/glove_mode";
+
+Return<bool> GloveMode::isEnabled() {
+    std::string val;
+
+    if (!android::base::ReadFileToString(kGloveModePath, &val)) {
+        PLOG(ERROR) << "Failed to read glove_mode";
+        return false;
+    }
+
+    android::base::Trim(val);
+    LOG(INFO) << "Enabled state raw: " << val;
+
+    // not sure if this is needed but it seemed to fail without it
+    if (val == "0x1") {
+        return true;
+    }
+
+    return false;
+}
+
+Return<bool> GloveMode::setEnabled(bool enabled) {
+    std::string val = enabled ? "1" : "0";
+
+    bool ok = android::base::WriteStringToFile(val, kGloveModePath);
+    if (!ok) {
+        PLOG(ERROR) << "Failed to write glove_mode";
+    }
+
+    LOG(INFO) << "NewEnabled state: " << enabled << " write_ok=" << ok;
+    return ok;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
